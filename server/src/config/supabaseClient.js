ef4452a535b952dd706762992ff94cb90339ec1b
manahillsajid <manahill.sajid@mail.utoronto.ca>
2025-10-04 21:12:35 -0400

    adding allyssa's supabase logic

---
+++ server/src/config/supabaseClient.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Use service key for full access (backend)
+const supabase = createClient(
+    process.env.SUPABASE_URL, 
+    process.env.SUPABASE_SERVICE_KEY
+);
+
+export default supabase;
